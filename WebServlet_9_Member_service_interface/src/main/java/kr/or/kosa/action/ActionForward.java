b21cc071b6b5e2d0af8604d931ebab7515f0465b
minaci1 <minaci_@naver.com>
2023-05-30 15:10:18 +0900

    변경 사항 없음

--- WebServlet_9_Member_service_interface/src/main/java/kr/or/kosa/action/ActionForward.java
+++ WebServlet_9_Member_service_interface/src/main/java/kr/or/kosa/action/ActionForward.java
@@ -9,24 +9,23 @@ package kr.or.kosa.action;
 화면 아니면 로직처리 
  */
 public class ActionForward {
-	private boolean isRedirect =false; //뷰의 전환 ( redirect or forward) 
+	private boolean isRedirect = false; // 뷰의 전환 ( redirect or forward)
 	private String path = null; // 이동경로 ( 뷰의 주소)
-	
-	
+
 	public boolean isRedirect() {
 		return isRedirect;
 	}
+
 	public void setRedirect(boolean isRedirect) {
 		this.isRedirect = isRedirect;
 	}
+
 	public String getPath() {
 		return path;
 	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
-	
-	
-	
+
 }
